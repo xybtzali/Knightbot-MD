52e0498bb4c60fac046d32c53ef4afbf39e92efb
xybtzali <kisilikgizli5@gmail.com>
2026-06-27 19:48:24 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "Whatsapp Bot",
   botOwner: 'Xybtzali', // Your name
   ownerNumber: '905539228984', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
